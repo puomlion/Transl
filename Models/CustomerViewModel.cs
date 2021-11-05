b8163e28f39e825185efb13e8bcda009a1f062b7
puomlion <sammydunamis@gmail.com>
2021-11-05 09:58:00 +0000

    first commit

---
+++ Models/CustomerViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebDemo.Models
+{
+    public class CustomerViewModel
+    {
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        [Display(Name = "Title")]
+        public string Title { get; set; }
+
+        [Display(Name = "Notes")]
+        public string Notes { get; set; }
+    }
+}
